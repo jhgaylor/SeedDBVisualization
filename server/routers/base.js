a069e10ac3344421a7311fad9d9c8b6012351338
Jake Gaylor <jhgaylor@gmail.com>
2015-04-30 07:59:45 -0500

    renamed app to server

--- server/routers/base.js
+++ server/routers/base.js
@@ -3,6 +3,8 @@ var BaseRouter = require('express').Router();
 BaseRouter.route('/')
   .get(function (req, res) {
     // serve the SPA
+    var data = {};
+    res.render('index.jade', data);
   });
 
 module.exports = BaseRouter;
