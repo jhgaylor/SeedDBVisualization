808a5946cfbe8fb2c543fb4f72d41d18980e6a70
Jake Gaylor <jhgaylor@gmail.com>
2015-04-30 08:00:30 -0500

    add jade support to app

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 // bootstrap the express application
-// Bootstraps the application and serve the express app
+var express = require('express');
 
 // import the routers
-var BaseRouter = require('./app/routers/base');
-var APIRouter = require('./app/routers/api');
+var BaseRouter = require('./server/routers/base');
+var APIRouter = require('./server/routers/api');
 // console.log(BaseRouter, APIRouter);
 // instantiate express
-var app = require('express')();
+var app = express();
 
 // grab a logging library
 var logger = require('morgan');
 
 // for json body parsing
 var bodyParser = require('body-parser');
+
+// use a templating engine
+app.set('view engine', 'jade');
+
+// configure template location
+app.set('views', './client/templates')
 
 // bind the logger to the express app
 app.use(logger("dev", {}));
