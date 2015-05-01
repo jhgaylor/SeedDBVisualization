677b895cbffec99f34a7f60c544d26456f828043
Jake Gaylor <jhgaylor@gmail.com>
2015-05-01 04:26:24 -0500

    api cleanup

--- client/static/main.js
+++ client/static/main.js
@@ -40,41 +40,41 @@ var Models = {
 var Slicers = {
   // returns a hashtable of {bracket_name: [accelerators]}
   // where brackets are:
-  //    "1B+": >$1B
-  //    "100M-1B": >$100M<$1B
-  //    "25M-100M": >$25M<$100M
-  //    "10M-25M": >$10M<$25M
-  //    "2M-10M": >$2M<$10M
-  //    "2M-": <$2M
+  //    0: >$1B
+  //    1: >$100M<$1B
+  //    2: >$25M<$100M
+  //    3: >$10M<$25M
+  //    4: >$2M<$10M
+  //    5: <$2M
   a_funding_brackets: function (accelerators) {
     var brackets = {
-      "1B+": [],
-      "100M-1B": [],
-      "25M-100M": [],
-      "10M-25M": [],
-      "2M-10M": [],
-      "2M-": [],
+      0: [],
+      1: [],
+      2: [],
+      3: [],
+      4: [],
+      5: [],
     };
     function numberToBracketKey (num) {
       if (num >= 1000000000) {
-        return "1B+";
+        return 0;
       } else if (num >= 100000000) {
-        return "100M-1B";
+        return 1;
       } else if (num >= 25000000) {
-        return "25M-100M";
+        return 2;
       } else if (num >= 10000000) {
-        return "10M-25M";
+        return 3;
       } else if (num >= 2000000) {
-        return "2M-10M";
+        return 4;
       } else {
-        return "2M-";
+        return 5;
       }
     }
     accelerators.forEach(function (accelerator) {
       // console.log(accelerator.funding);
       // console.log(accelerator.funding.replace(/,/g, ''));
       // console.log(accelerator.funding);
-      var funding = Number(accelerator.funding.replace(/,/g, ''));
+      var funding = accelerator.fundingNumber
       // don't display accelerators that haven't funded anything.
       if (funding < 1) {
         return;
@@ -163,7 +163,7 @@ var APIWrapper = {
   .catch(function(err) {
     console.log(err);
   });
-})() // the first comment on this line prevents the examples from running
+});//(); // the first comment on this line prevents the examples from running
 
 // draw the appropriate visualization - use routing?
 // click events on visualization
