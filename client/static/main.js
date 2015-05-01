9b5cc61a876914d1fd8010500b07f63bfc10c160
Jake Gaylor <jhgaylor@gmail.com>
2015-05-01 00:58:23 -0500

    create client side api consumer
    We needed a way to ask for data from the server. Since we’re only ever
    reading we don’t have to have anything very complex. I decided to just
    roll my own to prevent unnecessary complexity. essentially i just
    wrapped jQuery.get in a promise and provide some ways to call that
    wrapper to get certain data.

--- client/static/main.js
+++ client/static/main.js
@@ -1,1 +1,49 @@
-console.log("Bower is ready", $, d3);
+// console.log("Bower is ready", $, d3);
+
+var APIWrapper = {
+  base_url: "http://localhost:3000/api",
+  get: function (url_partial) {
+    var def = Q.defer();
+    var full_url = this.base_url + url_partial;
+    $.get(full_url)
+      .done(function (data) {
+        def.resolve(data);
+      })
+      .fail(function () {
+        def.reject({error: "AJAX Failed.", url: full_url});
+      });
+    return def.promise;
+  },
+  getAccelerators: function () {
+    var p = this.get('/accelerators');
+    console.log(p);
+    return p;
+  },
+  getAcceleratorCompanies: function (acceleratorId) {
+    return this.get('/accelerators/'+acceleratorId);
+  },
+  getExits: function (company) {
+    return this.get('/exits');
+  }
+};
+
+APIWrapper.getAccelerators().then(function (accelerators) {
+  console.log("Accelerators", accelerators);
+})
+.catch(function(err) {
+  console.log(err);
+});
+
+APIWrapper.getAcceleratorCompanies(1011).then(function (companies) {
+  console.log("companies", companies);
+})
+.catch(function(err) {
+  console.log(err);
+});
+
+APIWrapper.getExits().then(function (companies) {
+  console.log("exits", companies);
+})
+.catch(function(err) {
+  console.log(err);
+});
